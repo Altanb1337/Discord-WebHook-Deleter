72882530098f00c64317e6b885c293f9e18b9667
HeySurfer <58826689+heysurfer@users.noreply.github.com>
2021-03-26 08:12:51 +0300

    Add files via upload

---
+++ WebHook Deleter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebHook_Deleter
+{
+    static class Program
+    {
+        //reqbin.com
+        static void Main()
+        {
+
+            Console.Write("WebHook : ");
+            var url = Console.ReadLine();
+            Console.Clear();
+            delete(url);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            Main();
+            
+
+        }
+        static void delete(string url)
+        {
+  
+            try
+            {
+                var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "DELETE";
+
+            httpRequest.Accept = "*/*";
+           
+                var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+                Console.WriteLine("Success, WebHook Deleted");
+            }
+            catch { wrongwebhook(); }
+        }
+        static void wrongwebhook()
+        {
+            Console.WriteLine("Unknown Webhook\ncode : 10015");
+        }
+    }
+}
